1c768e82dcb1e6abe61a1afd218fa6912b85db18
Adriel de Souza <ds.adriel@gmail.com>
2026-04-02 09:25:33 -0300

    Add xmlEscape function and update lastmod dates in sitemap

--- .github/scripts/generate-sitemap.js
+++ .github/scripts/generate-sitemap.js
@@ -25,6 +25,19 @@ function getSupportPages() {
     return Object.keys(supportInfo).map(bundle => `/suporte/?bundle=${bundle}`);
 }
 
+function xmlEscape(str) {
+    return str.replace(/[<>&"']/g, function (m) {
+        switch (m) {
+            case '<': return '&lt;';
+            case '>': return '&gt;';
+            case '&': return '&amp;';
+            case '"': return '&quot;';
+            case "'": return '&apos;';
+            default: return m;
+        }
+    });
+}
+
 function generateSitemap() {
     const allPages = [
         ...staticPages,
@@ -35,7 +48,7 @@ function generateSitemap() {
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${allPages.map(page => `  <url>
-    <loc>${BASE_URL}${page}</loc>
+    <loc>${xmlEscape(BASE_URL + page)}</loc>
     <lastmod>${new Date().toISOString().split('T')[0]}</lastmod>
     <changefreq>monthly</changefreq>
     <priority>${page === '/' ? '1.0' : '0.8'}</priority>
